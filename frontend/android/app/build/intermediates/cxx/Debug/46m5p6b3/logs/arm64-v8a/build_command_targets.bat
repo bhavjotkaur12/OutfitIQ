2d83aca4f024aafc2fd0d5369fe652ff1bc9ac64
Bhavjotkaur12 <bhavjotkaur12@gmail.com>
2025-05-09 15:40:28 -0400

    Remove large build files and add to .gitignore
    Former-commit-id: 3ee24133d5726326f867ecf73e1553f10a5ec496

--- frontend/android/app/build/intermediates/cxx/Debug/46m5p6b3/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\Concierge\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\capstone\\frontend\\android\\app\\.cxx\\Debug\\46m5p6b3\\arm64-v8a" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
--- frontend/android/app/build/intermediates/cxx/Debug/46m5p6b3/logs/x86_64/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\Concierge\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\capstone\\frontend\\android\\app\\.cxx\\Debug\\46m5p6b3\\x86_64" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
